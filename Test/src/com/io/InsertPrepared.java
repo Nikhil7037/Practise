e2285d434e382f56606fec62f63aeb229ffc751c
HP <HP@DESKTOP-2V938C9>
2020-02-02 00:02:05 +0530

    First push in my reporsitory

---
+++ Test/src/com/io/InsertPrepared.java
@@ -0,0 +1,27 @@
+package com.io;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class InsertPrepared {
+	public static void main(String args[]){ 
+		try{  
+		Class.forName("oracle.jdbc.driver.OracleDriver");  
+		  
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","system","password");  
+		  
+		PreparedStatement stmt=con.prepareStatement("insert into emp1 values(?,?)");  
+		stmt.setInt(1,101);//1 specifies the first parameter in the query  
+		stmt.setString(2,"Ratan");  
+		  
+		int i=stmt.executeUpdate();  
+		System.out.println(i+" records inserted");  
+		  
+		con.close();  
+		  
+		}catch(Exception e){ System.out.println(e);}  
+		  
+		}  
+
+}
